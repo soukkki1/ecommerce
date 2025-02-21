128f46101b77c2b1b9080903774513e108819331
soukainag <soukaina.guassmi@gmail.com>
2025-02-21 18:08:37 +0100

    vault Set up

--- user-service/src/main/java/com/soukaina/user/config/SecurityConfig.java
+++ user-service/src/main/java/com/soukaina/user/config/SecurityConfig.java
@@ -1,76 +1,26 @@
 package com.soukaina.user.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Configuration
-@EnableMethodSecurity
-@ConditionalOnProperty(name = "app.features.enable-auth", havingValue = "true", matchIfMissing = true)
+@EnableWebSecurity
 public class SecurityConfig {
-    private static final String[] AUTH_WHITELIST = {
-    };
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    private String jwkSetUri;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        auth ->
-                                auth.requestMatchers(AUTH_WHITELIST)
-                                        .permitAll()
-                                        .anyRequest()
-                                        .authenticated()
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt
-                        .jwkSetUri(jwkSetUri)
-                        .jwtAuthenticationConverter(jwtAuthenticationConverter())
-                ));
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
-
-    private JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
-        return jwtConverter;
-    }
-
-
-    @RequiredArgsConstructor
-    public static class KeycloakRoleConverter
-            implements Converter<Jwt, Collection<GrantedAuthority>> {
-        private static final String ROLES_CLAIM = "roles";
-        private static final String REALM_ACCESS_CLAIM = "realm_access";
-
-        @Override
-        public Collection<GrantedAuthority> convert(final Jwt source) {
-            final Map<String, List<String>> realmAccess = source.getClaim(REALM_ACCESS_CLAIM);
-            final List<String> rolesAsString = new ArrayList<>(realmAccess.get(ROLES_CLAIM));
-
-            return rolesAsString.stream()
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
-        }
-    }
-
 }
+
