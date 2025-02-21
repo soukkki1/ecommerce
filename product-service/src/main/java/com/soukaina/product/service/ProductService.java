4b191875a03c105f8de13e8cbb11581894d60e8b
soukainag <soukaina.guassmi@gmail.com>
2025-02-21 10:53:53 +0100

    GatewayAndKeycloack

---
+++ product-service/src/main/java/com/soukaina/product/service/ProductService.java
@@ -0,0 +1,30 @@
+package com.soukaina.product.service;
+
+import com.soukaina.product.model.Product;
+import com.soukaina.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Cacheable(value = "products", key = "#id")  // Caches product by ID
+    public Product getProductById(Long id) {
+        System.out.println("Fetching from DB: Product ID " + id);
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    @Cacheable(value = "allProducts")  // Caches all products
+    public List<Product> getAllProducts() {
+        System.out.println("Fetching all products from DB");
+        return productRepository.findAll();
+    }
+}
--- user-service/src/main/java/com/soukaina/user/config/SecurityConfig.java
+++ user-service/src/main/java/com/soukaina/user/config/SecurityConfig.java
@@ -1,37 +1,76 @@
 package com.soukaina.user.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Configuration
+@EnableMethodSecurity
+@ConditionalOnProperty(name = "app.features.enable-auth", havingValue = "true", matchIfMissing = true)
 public class SecurityConfig {
+    private static final String[] AUTH_WHITELIST = {
+    };
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())  // Disable CSRF for APIs
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("api/public").permitAll()  // Public endpoints
-                        .requestMatchers("api/admin").hasRole("ADMIN")  // Only ADMIN can access
-                        .requestMatchers("api/user").hasAnyRole("USER", "ADMIN")  // USER & ADMIN roles
-                        .anyRequest().authenticated()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth ->
+                                auth.requestMatchers(AUTH_WHITELIST)
+                                        .permitAll()
+                                        .anyRequest()
+                                        .authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
-
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt
+                        .jwkSetUri(jwkSetUri)
+                        .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                ));
         return http.build();
     }
 
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("realm_access.roles"); // Keycloak stores roles under realm_access
-        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
-        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
-        authenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
-        return authenticationConverter;
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return jwtConverter;
     }
+
+
+    @RequiredArgsConstructor
+    public static class KeycloakRoleConverter
+            implements Converter<Jwt, Collection<GrantedAuthority>> {
+        private static final String ROLES_CLAIM = "roles";
+        private static final String REALM_ACCESS_CLAIM = "realm_access";
+
+        @Override
+        public Collection<GrantedAuthority> convert(final Jwt source) {
+            final Map<String, List<String>> realmAccess = source.getClaim(REALM_ACCESS_CLAIM);
+            final List<String> rolesAsString = new ArrayList<>(realmAccess.get(ROLES_CLAIM));
+
+            return rolesAsString.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        }
+    }
+
 }
